fbc58c2efa6f24bcaa7ecd3018946b6964c0a1c3
JohnyTheRipper <121682738+JohnyTheRipper@users.noreply.github.com>
2024-04-18 21:25:32 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript for responsive menu
-const toggleMenu = document.querySelector('.toggle-menu');
-const navigation = document.querySelector('.navigation ul');
-
-toggleMenu.addEventListener('click', () => {
-    navigation.classList.toggle('show');
-});
